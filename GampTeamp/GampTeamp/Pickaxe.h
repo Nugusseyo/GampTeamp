c5a3796481fbf6ff0ba37d20f3d01a8d27f72d31
ArnoNonim <Radpids2@gmail.com>
2026-06-18 10:21:31 +0900

    feat : PickaxeUpgrade

--- GampTeamp/GampTeamp/Pickaxe.h
+++ GampTeamp/GampTeamp/Pickaxe.h
@@ -1,8 +1,82 @@
-#pragma once
+﻿#pragma once
 #include "Console.h"
+#include "GameState.h"
 
-extern string pickaxeName;
-extern Color pickaxeColor;
+static vector<int> pickaxeWidthCache;   
 
+struct PickaxeData
+{
+    string name;
+    Color color;
+    vector<wstring> art;
+};
+
+static vector<PickaxeData> pickaxeTable =
+{
+    {
+        "낡아빠진 곡괭이", Color::GRAY,
+        {
+            L"   ⠀⢀⣀⣀⣀⣀⠀⠀⠀⠀",
+            L"⠀⠀⠀⣾⣭⣭⣭⡍⣶⣶⡆⠀",
+            L"⠀⠀⠀⠈⠋⠉⣩⣾⣀⠿⣇⠀",
+            L"⠀⠀⠀⠀⣠⣾⣿⠟⢹⡿⣿⡀",
+            L"⠀⠀⣠⣾⣿⠏⠁⠀⠸⢟⡿⠄",
+            L"⢀⣸⣿⡧⠛⠀⠀⠀⠀⠘⠃⠀",
+            L"⠘⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        }
+    },
+    {
+        "그냥 곡괭이", Color::WHITE,
+        {
+            L"⠀⠀⠀⠐⢆⡀⠀⠀⠀⠀⠀",
+            L"⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀",
+            L"⠀⠀⠀⠀⠀⢀⡾⣷⢆⠀⠀",
+            L"⠀⠀⠀⠀⣠⣿⠏⠀⠀⠓⠄",
+            L"⠀⠀⢀⣴⡟⠁⠀⠀⠀⠀⠈",
+            L"⠀⣠⣿⠋⠀⠀⠀⠀⠀⠀⠀",
+            L"⠌⡽⠁⠀⠀⠀⠀⠀⠀⠀⠀",
+        }
+    },
+    {
+        "간지나는 곡괭이", Color::CYAN,
+        {
+            L"⠀⠀⠀⠀⢰⣶⣴⣆⡰⠀⠀⠀",
+            L"⠀⠀⠀⠀⠀⠉⠛⣿⣿⣶⡆⠀",
+            L"⠀⠀⠀⠀⠀⠀⢠⣾⣿⣯⣤⠀",
+            L"⠀⠀⠀⠀⠀⣠⠟⠁⠙⠿⠻⢇",
+            L"⠀⠀⠀⢠⠞⠁⠀⠀⠀⠀⠀⠀",
+            L"⠀⠀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀",
+            L"⠐⠚⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+        }
+    },
+    {
+        "개쩌는 곡괭이", Color::YELLOW,
+        {
+            L"⠀⠀⢀⡠⠠⠀⠀⠀",
+            L"⠀⠀⠐⣾⣧⡄⠀⠀",
+            L"⠀⠀⠀⣻⣿⠿⣦⠀",
+            L"⠀⠀⢺⠟⠀⠀⠈⠄",
+            L"⠀⠀⠊⠀⠀⠀⠀⠀",
+            L"⠀⡘⠀⠀⠀⠀⠀⠀",
+            L"⠐⠃⠀⠀⠀⠀⠀⠀",
+        }
+    },
+    {
+        "네.더.라.이.트. 곡괭이", Color::VIOLET,
+        {
+            L"⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀",
+            L"⠀⠀⠈⠈⠛⣯⣀⠀⠀⠀⠀",
+            L"⠀⠀⠀⠀⢐⢫⡿⠛⢿⡆⠀",
+            L"⠀⠀⠀⠀⣸⠇⠁⠀⠀⠿⡆",
+            L"⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⠡",
+            L"⠀⢀⠞⠀⠀⠀⠀⠀⠀⠀⠀",
+            L"⢠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        }
+    }
+};
+
+void ClearPickaxe(int x, int y);
+void SetPickaxeLevel(int level);
 void RenderPickaxe(int x, int y);
 void RenderPickaxeName();
+void InitPickaxeCache();
